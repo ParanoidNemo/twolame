f0e39defefcf91aa6e6ff61ac3201a96d1ae4ed2
Andrea Calzavacca <paranoid.nemo@gmail.com>
2015-09-28 14:59:42 +0200

    first working output - yey

--- twolame.py
+++ twolame.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #   Copyright (C) 2015 by Andrea Calzavacca <paranoid.nemo@gmail.com>
 #
 #   This program is free software; you can redistribute it and/or modify
@@ -15,13 +17,54 @@
 #   Free Software Foundation, Inc.,
 #   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#!/usr/bin/env python
-
 #import std module(s)
 import os, sys, re
 import threading
 
 #import custom module(s)
-import music    
+import filesystem
 
-class MPD(threading.Thread):
+#Replacement for string.format
+def insert_data(string, rep_dict):
+    pattern = re.compile("|".join([re.escape(k) for k in rep_dict.keys()]), re.M)
+    return pattern.sub(lambda x: rep_dict[x.group(0)], string)
+
+class FS(threading.Thread):
+
+    def __init__(self, threadID):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.outstring = ''
+        self.format_string = ''
+
+        try:
+            os.mkfifo(os.path.expanduser('~/.local/share/be.shell/fifo/twolame'))
+        except:
+            pass
+
+        with open(os.path.expanduser('~/.kde4/share/apps/be.shell/Themes/Hydrogen/twolame/fs.format')) as f:
+            for line in f:
+                if line.startswith('#'):
+                    continue
+                self.format_string += line
+
+        self.format_string = re.sub(r'>\s<', '><', self.format_string)
+
+    def run(self):
+        while True:
+            self.out()
+
+    def out(self):
+        self.info = filesystem.info
+        self.outstring = insert_data(self.format_string, self.info)
+
+        with open(os.path.expanduser('~/.local/share/be.shell/fifo/twolame'), 'w') as f:
+            f.write(self.outstring)
+            f.write('\n')
+
+def main():
+    fit1 = FS(1)
+    fit1.start()
+
+if __name__ == '__main__':
+    main()
