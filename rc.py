800dac7a520d967d6c06e7ec672ebc43bc1c2759
Andrea Calzavacca <paranoid.nemo@gmail.com>
2015-10-01 22:05:18 +0200

    now filestystem create a list instead of a string

--- rc.py
+++ rc.py
@@ -4,11 +4,31 @@ import os
 import configparser
 
 def get_rc(rc_file):
-    global FILESYSTEM, FS_UPDATE_PERIOD, MNT1, MNT2, CSS
+
+    # define the global var
+    global CSS
+    global FILESYSTEM, FS_UPDATE_PERIOD, MNT
+
+    # create the parser
     conf_parser = configparser.ConfigParser()
     rc = conf_parser.read(rc_file)
+
+    # set the CSS file
+    CSS = conf_parser.get('twolame', 'css_file')
+
+    # gather info for fs module
     FILESYSTEM = conf_parser.get('twolame', 'start_fs')
     FS_UPDATE_PERIOD = conf_parser.get('fs', 'update_period')
-    MNT1 = conf_parser.get('fs', 'mount_point1')
-    MNT2 = conf_parser.get('fs', 'mount_point2')
-    CSS = conf_parser.get('twolame', 'css_file')
+    MNT_NUM = conf_parser.get('fs', 'mnt_num')
+    try:
+        _MNT_NUM = int(MNT_NUM)
+        MNT = []
+        for i in range(_MNT_NUM):
+            _i = str(i)
+            if conf_parser.get('fs', 'mount_point' + _i) != '':
+                MNT.append(conf_parser.get('fs', 'mount_point' + _i))
+            else:
+                pass
+
+    except Exception as ex:
+        print(ex)
