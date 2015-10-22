7ae5e70a73e591bf0ab3d86faa26626093449066
Andrea Calzavacca <paranoid.nemo@gmail.com>
2015-10-22 15:42:12 +0200

    add cloud info module

---
+++ cloud.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+#   Copyright (C) 2015 by Andrea Calzavacca <paranoid.nemo@gmail.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the
+#   Free Software Foundation, Inc.,
+#   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+import os, sys, re
+
+import rc
+from spam import clouds
+from spam import beshell
+
+rc_file = os.path.join(beshell.Theme.path(), 'twolamerc')
+
+rc.get_rc(rc_file)
