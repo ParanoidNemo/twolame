86a447203591cbe3faa09991ae8f1269a0c9c475
Andrea Calzavacca <paranoid.nemo@gmail.com>
2015-11-11 14:22:30 +0100

    sync with system update

--- rc.py
+++ rc.py
@@ -43,18 +43,17 @@ def get_rc(rc_file):
     # set the CSS file
     CSS = conf_parser.get('twolame', 'css_file')
 
-    # gather info for fs module
-    FILESYSTEM = conf_parser.get('twolame', 'start_fs')
-    FM = conf_parser.get('fs', 'fm')
-    FS_UPDATE_PERIOD = conf_parser.get('fs', 'update_period')
-    MNT_NUM = conf_parser.get('fs', 'mnt_num')
+    # gather info for system module
+    FILESYSTEM = conf_parser.get('twolame', 'start_system')
+    FM = conf_parser.get('system', 'fm')
+    FS_UPDATE_PERIOD = conf_parser.get('system', 'update_period')
+    MNT_NUM = conf_parser.get('system', 'mnt_num')
     try:
-        _MNT_NUM = int(MNT_NUM)
         MNT = []
-        for i in range(_MNT_NUM):
+        for i in range(int(MNT_NUM)):
             _i = str(i)
-            if conf_parser.get('fs', 'mount_point' + _i) != '':
-                MNT.append(conf_parser.get('fs', 'mount_point' + _i))
+            if conf_parser.get('system', 'mount_point' + _i) != '':
+                MNT.append(conf_parser.get('system', 'mount_point' + _i))
             else:
                 pass
 
